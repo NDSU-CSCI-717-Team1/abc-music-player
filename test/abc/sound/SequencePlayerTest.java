bb312e626c424203882f75510438f351460f0595
Keinan Balsam <sanchewy@gmail.com>
2022-11-20 13:06:32 -0700

    Fix test cases

--- test/abc/sound/SequencePlayerTest.java
+++ test/abc/sound/SequencePlayerTest.java
@@ -21,37 +21,17 @@ public class SequencePlayerTest {
 	@Test
 	public void test() {
 		String fileName = "sample_abc/piece1.abc";
-
 		try {
-			// Open the input file stream
-			CharStream input = fromFileName(fileName);  //load the file
-
-			// Create a lexer that feeds off of input CharStream
+			CharStream input = fromFileName(fileName);
 			AbcLexer lexer = new AbcLexer(input);
-
-			// Create a buffer of tokens pulled from the lexer
 			CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-			// Create a parser that feeds off the tokens buffer
 			AbcParser parser = new AbcParser(tokens);
-
-			// Begin parsing at rule abc_tune
 			ParseTree tree = parser.abc_tune();
-
-			// Create a generic parse tree walker that can trigger callbacks
+			// TODO: Implement custom walker, parse tree into POJO, play notes using SquencePlayer
 			ParseTreeWalker walker = new ParseTreeWalker();
 			AbcBaseListener listener = new AbcBaseListener();
-			// Walk the tree created during the parse, trigger callbacks
 			walker.walk(listener, tree);
-			System.out.println(); // print a \n after translation
-
-			// Walk the tree again to translate to java
-			walker.walk(listener, tree);
-
-			// Print LISP-style tree
-			// System.out.println(tree.toStringTree(parser));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
